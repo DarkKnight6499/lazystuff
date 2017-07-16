7b807b18d28b3ec8d7bb8e990d964e9ff924dbc5
DarkKnight6499 <yazad.madan@gmail.com>
2017-07-16 13:11:54 +0000

    Initial

---
+++ mido/aos.sh
@@ -0,0 +1,11 @@
+
+git clone https://github.com/TeamDarkness-Devices/device_xiaomi_mido -b n-mr2 device/xiaomi/mido
+
+git clone https://github.com/TeamDarkness-Devices/proprietary_vendor_xiaomi -b cm-14.1 vendor/xiaomi
+
+git clone https://github.com/AKabhishek/kernel_xiaomi_msm8953 -b nucleon kernel/xiaomi/msm8953
+
+git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b cm-14.1 package$
+
+
+
---
+++ mido/dnd.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/TeamDarkness-Devices/device_xiaomi_mido -b n7x device/xiaomi/mido
+
+git clone https://github.com/TeamDarkness-Devices/proprietary_vendor_xiaomi -b cm-14.1 vendor/xiaomi
+
+git clone https://github.com/AKabhishek/kernel_xiaomi_msm8953 -b nucleon kernel/xiaomi/msm8953
+
+git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b cm-14.1 packages/resources/devicesettings
+
+
+
